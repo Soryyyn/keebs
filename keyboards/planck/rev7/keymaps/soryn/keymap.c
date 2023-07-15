e27c941cb5b4ab356bae48632d9dac2cb83cb4f2
Soryn Bächli <soryn.baechli@gmail.com>
2023-07-15 14:14:51 +0200

    feat: Add tap-dance for copy/paste/cut
    This commit adds a tap-dance func to copy/paste/cut based on amount of times pressed (1, 2, 3).

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -15,6 +15,30 @@ enum planck_keycodes { QWERTY = SAFE_RANGE, BACKLIT };
 #define UML_OE RALT(KC_P)
 #define UML_UE RALT(KC_Y)
 
+/*
+ * Tap-dance configuration.
+ */
+enum {
+    TD_CPC = 0
+}
+
+// Switch between copy / paste / cut.
+void copy_paste_cut (qk_tap_dance_state_t *state, void *user_data) {
+    if(state->count == 1) {
+        tap_code(C(KC_C));
+    } else if(state->count == 2) {
+        tap_code(C(KC_V));
+    } else if(state->count == 3) {
+        tap_code(C(KC_X));
+    } else {
+        reset_tap_dance(state);
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CPC] = ACTION_TAP_DANCE_FN(copy_paste_cut);
+}
+
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |  Z   |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  | Copy |Lower |    Space    |Raise |Enter | Left | Down |Right |
+ * | Ctrl | Alt  | GUI  | CPC  |Lower |    Space    |Raise |Enter | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-     KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-     KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LALT, KC_LGUI, C(KC_C),   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
+     KC_ESC,    KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_TAB,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,    KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
+    KC_LCTL, KC_LALT, KC_LGUI, TD(TD_CPC),   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
@@ -44,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      | Num/ | Num* | Num- | Num+ |  [   |  ]   | Num1 | Num2 | Num3 | Pgup | Num= |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |Paste |      |             |      | NumE | Home | Pgdn | End  |
+ * |      |      |      |      |      |             |      | NumE | Home | Pgdn | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
      KC_DEL, A(KC_1), A(KC_2), A(KC_3), A(KC_4), S(KC_9), S(KC_0),   KC_P7,   KC_P8,   KC_P9,   KC_P0, _______,
     _______,  KC_GRV, KC_BSLS,  KC_EQL, KC_MINS,S(KC_COMM),S(KC_DOT),   KC_P4,   KC_P5,   KC_P6, KC_PDOT, KC_NUM,
     _______, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_LBRC, KC_RBRC,   KC_P1,   KC_P2,   KC_P3, KC_PGUP, KC_PEQL,
-    _______, _______, _______, C(KC_V), _______, _______, _______, _______, KC_PENT, KC_HOME,KC_PGDN,  KC_END
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PENT, KC_HOME,KC_PGDN,  KC_END
 ),
 
 /* Raise
@@ -62,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |   Ä  |   Ö  |   Ü  |      |      |      |      |      |      | Vol+ | Play |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      | Cut  |      |             |      |      | Play | Vol- | Next |
+ * |      |      |      |      |      |             |      |      | Play | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     _______,  UML_AE,  UML_OE,  UML_UE, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_MPLY,
-    _______, _______, _______, C(KC_X), _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_MNXT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_MNXT
 ),
 
 /* Adjust (Lower + Raise)
