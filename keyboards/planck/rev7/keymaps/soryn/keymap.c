d2cb3c1384123b32998228401ab73eb3a1f18ee5
Soryn Bächli <soryn.baechli@gmail.com>
2023-07-17 14:21:08 +0200

    feat: Trying mod-tap copy/paste

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -14,6 +14,8 @@ enum planck_keycodes { QWERTY = SAFE_RANGE, BACKLIT };
 #define UML_OE RALT(KC_P)
 #define UML_UE RALT(KC_Y)
 
+#define MT_COPA LT(0, KC_NO)
+
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -32,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_GESC,    KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
      KC_TAB,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LALT, KC_LGUI,      KC_NO,   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI,    MT_COPA,   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
@@ -121,7 +123,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         // Single copy/paste key.
         // Copy on tap / paste on hold.
-        case LT(0, KC_NO):
+        case MT_COPA:
             if (record->tap.count && record->event.pressed) {
                 tap_code16(C(KC_C));
             } else if (record->event.pressed) {
