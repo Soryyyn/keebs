4c61274a259f65697ed2fde17da0297202411a5e
Soryn Bächli <soryn.baechli@gmail.com>
2023-08-02 17:24:28 +0200

    feat: Working on adding/changing `Numbers`/`Symbols` layers

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -5,9 +5,10 @@
 #include "os_detection.h"
 #include "functions.c"
 
-enum planck_layers { _BASE, _LOWER, _RAISE, _ADJUST, _FN_KEYS };
+enum planck_layers { _BASE, _LOWER, _NUMBERS, _RAISE, _ADJUST, _FN_KEYS };
 
 #define LOWER MO(_LOWER)
+#define NUMS MO(_NUMBERS)
 #define RAISE MO(_RAISE)
 #define FN_KEYS OSL(_FN_KEYS)
 
@@ -16,29 +17,26 @@ enum planck_layers { _BASE, _LOWER, _RAISE, _ADJUST, _FN_KEYS };
 #define UML_UE RALT(KC_Y)
 
 #define MT_COPA LT(0, KC_NO)
-#define MT_A LT(0, KC_A)
-#define MT_O LT(0, KC_O)
-#define MT_U LT(0, KC_U)
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base
  * ,-----------------------------------------------------------------------------------.
- * | Esc/~|  Q   |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc  |  Q   |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  | A/Ä  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ;/:  | '/"  |
+ * | Tab  |  A   |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ;/:  | '/"  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |  Z   |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  | //?  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |CO/PA |Lower |    Space    |Raise |Enter | Left | Down |Right |
+ * | Ctrl | Alt  | GUI  |CO/PA | NUMS |    Space    |Raise |Enter | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-    QK_GESC,    KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-     KC_TAB,    MT_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_ESC,    KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_TAB,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LALT, KC_LGUI,    MT_COPA,   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI,    MT_COPA,    NUMS,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
@@ -57,6 +55,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_DLR,   KC_AT, KC_LBRC, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_RBRC, KC_PERC, KC_HASH, KC_CIRC,
     _______, _______, _______, _______, KC_EXLM, KC_AMPR, KC_BSLS, KC_ASTR, _______, _______, KC_PGUP, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, FN_KEYS, KC_HOME, KC_PGDN,  KC_END
+),
+
+/* Numbers
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |  *   |  -   |  {   |  }   |      |  7   |  8   |  9   |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |  /   |  +   |  (   |  )   |      |  4   |  5   |  6   |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |  .   |  [   |  ]   |      |  1   |  2   |  3   |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |  0   |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NUMBERS] = LAYOUT_planck_grid(
+    _______, _______, KC_PAST, KC_PMNS, KC_LCBR, KC_RCBR, _______,   KC_P7,   KC_P8,   KC_P9, _______, _______,
+    _______, _______, KC_PSLS, KC_PPLS, KC_LPRN, KC_RPRN, _______,   KC_P4,   KC_P5,   KC_P6, _______, _______,
+    _______, _______, _______, KC_PDOT, KC_LBRC, KC_RBRC, _______,   KC_P1,   KC_P2,   KC_P3, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,   KC_P0, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -153,17 +169,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
 
-            return false;
-
-        // Umlaut mod-taps
-        // tap -> a / hold -> ä
-        case MT_A:
-            simpleOnPressAndHold(record, KC_A, UML_AE);
-        // tap -> o / hold -> ö
-        case MT_O:
-            return false;
-        // tap -> u / hold -> ü
-        case MT_U:
             return false;
     }
 
