cc551ff83bd0c58d3c993ab1bd54a80eae7ba26c
Soryn Bächli <soryn.baechli@gmail.com>
2023-08-02 17:36:52 +0200

    feat: Temporary disabling new nums layer

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -18,6 +18,12 @@ enum planck_layers { _BASE, _LOWER, _NUMBERS, _RAISE, _ADJUST, _FN_KEYS };
 
 #define MT_COPA LT(0, KC_NO)
 
+//
+// Tap-dance declarations.
+// enum {
+//     TD
+// }
+
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -36,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,    KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
      KC_TAB,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LALT, KC_LGUI,    MT_COPA,    NUMS,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI,    MT_COPA,   LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
