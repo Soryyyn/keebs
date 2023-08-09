2095609c2eeed1cc4f613801bd66baf85eb55aad
Soryn Bächli <soryn.baechli@gmail.com>
2023-08-09 17:44:24 +0200

    feat: Added hyper on esc mod-tap

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -18,15 +18,16 @@ enum layers {
 
 //
 // Special keys.
-#define SP_COPA LT(_BASE, KC_NO)
+#define SP_COPA LT(_BASE, KC_NO) // Copy / paste
 #define SP_0SYM LT(_SYMBOLS, KC_0) // Symbol layer on hold / 0 on press.
+#define SP_HYES MT(MOD_HYPR, KC_ESC) // Hyper on escape hold / normal escape on press (Hyper = CTRL & Shift & Alt & GUI)
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   | Bksp |
+ * |Hy/Esc|  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   | ;/:  | '/"  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -36,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-     KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+    SP_HYES,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_SLSH,
     KC_LCTL, KC_LALT, KC_LGUI, SP_COPA,    NUMS,  KC_SPC,  KC_SPC,    SYMS,  KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT
