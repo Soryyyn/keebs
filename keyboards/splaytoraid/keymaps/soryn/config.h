5f8a99c446b4553ab1895bb6d849b98708c26fa4
Soryn Bächli <soryn.baechli@gmail.com>
2025-04-05 18:07:04 +0200

    feat: Remove old workflow + able to create 16 layers

--- keyboards/splaytoraid/keymaps/soryn/config.h
+++ keyboards/splaytoraid/keymaps/soryn/config.h
@@ -5,3 +5,6 @@
 #define VIAL_KEYBOARD_UID {0xCB, 0x00, 0x04, 0x00, 0x20, 0x04, 0xCA, 0xFE}
 #define VIAL_UNLOCK_COMBO_ROWS { 5, 5 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+// Able to create 16 layers.
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
