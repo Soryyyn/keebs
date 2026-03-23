cdb89aed85cf1313a6718a96c335bb87beca8ed5
Soryn Bächli <contact@soryn.dev>
2026-03-23 15:47:09 +0100

    feat: replace copy/paste key with Hyper

--- keyboards/planck/rev7/keymaps/soryn/keymap.c
+++ keyboards/planck/rev7/keymaps/soryn/keymap.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "os_detection.h"
 
 enum layers {
     _BASE,
@@ -17,7 +16,6 @@ enum layers {
 
 //
 // Special keys.
-#define SP_COPA LT(_BASE, KC_NO) // Copy / paste
 #define SP_0SYM LT(_SYMBOLS, KC_0) // Symbol layer on hold / 0 on press.
 #define SP_HYES MT(MOD_HYPR, KC_ESC) // Hyper on escape hold / normal escape on press (Hyper = CTRL & Shift & Alt & GUI)
 
@@ -32,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |  Z   |  X   |  C   |  V   |  B   |  N   |  M   | ,/<  | ./>  |  Up  | //?  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |CO/PA |NUM/SY|    Space    | SYM  |Enter | Left | Down |Right |
+ * | Ctrl | Alt  | GUI  |Hyper |NUM/SY|    Space    | SYM  |Enter | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
     SP_HYES,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, KC_SLSH,
-    KC_LCTL, KC_LALT, KC_LGUI, SP_COPA,    NUMS,  KC_SPC,  KC_SPC,    SYMS,  KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LALT, KC_LGUI, KC_HYPR,    NUMS,  KC_SPC,  KC_SPC,    SYMS,  KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Numbers & Mouse emu.
